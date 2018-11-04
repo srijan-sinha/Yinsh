a7405ada105ff1488063bdb4bc7a3927d98204f8
Siddhant Shingi <snshingi19@gmail.com>
2018-11-04 17:56:10 +0530

    first attempt

--- Bot/src/YinshBot.cpp
+++ Bot/src/YinshBot.cpp
@@ -258,60 +258,79 @@ int YinshBot::ringAttacks() {
 	return count;
 }
 
-double YinshBot::evalFunction () {
-	// double weight1 = 2;
-	// double weight2 = -3;
-	// double weight3 = 20;
-	// double weight4 = -15;
-	// double weight5 = 5000;
-	// double weight6 = -3000;
-	// double weight7 = 2;
-	// double weight8 = -1;
-	// double weight9 = 35;
-	// double weight10 = 15;
-	// double weight11 = -12;
-	// double weight12 = 0;
-	// double weight13 = 0;
-	// double weight14 = 0;
-	// double weight15 = 0;
+vector<double> YinshBot::getWeights () {
+	vector<double> w;
+	w.push_back(500);
+	w.push_back(-450);
+	w.push_back(100);
+	w.push_back(-90);
+	w.push_back(1000000);
+	w.push_back(-1000000);
+	w.push_back(5);
+	w.push_back(-4);
+	w.push_back(50);
+	w.push_back(50000);
+	w.push_back(-50000);
+	w.push_back(500);
+	w.push_back(-500);
+	w.push_back(1000);
+	w.push_back(-1000);
+	return w;
+}
 
-	double weight1 = 500;
-	double weight2 = -450;
-	double weight3 = 100;
-	double weight4 = -90;
-	double weight5 = 1000000;
-	//for us as player 2
-	double weight6 = -1000000;
-	//for us as player 1
-	// double weight6 = -5000000;
-	double weight7 = 5;
-	double weight8 = -4;
-	double weight9 = 50;
-	double weight10 = 50000;
-	double weight11 = -50000;
-	double weight12 = 500;
-	double weight13 = -500;
-	double weight14 = 1000;
-	double weight15 = -1000;
+vector<double> YinshBot::getFeatures (double &min, double &max) {
+	vector<double> f;
 
-	double eval = 0;
-
-	if(board->getNumRings() <= maxRings && board->getRingsScored() == 0 && board->getNumDiscs()==0) {
+	double eval1 = 0,eval2 = 0,eval3 = 0,eval4 = 0,eval5 = 0,eval6 = 0,eval7 = 0,eval8 = 0,eval9 = 0,eval10 = 0,eval11 = 0,eval12 = 0,eval13 = 0,eval14 = 0,eval15 = 0;
+	if(board->getNumRings() <= maxRings && board->getRingsScored() == 0 && board->getNumDiscs()==0) 
+	{
 		int h, p = 1;
 		for(int i = 0; i < board->getNumRings(); i++) {
 			board->convertBack(getRingVLR(1, 1, i), getRingVLR(1, 2, i), getRingVLR(1, 3, i), h, p);
-			eval = eval + (weight7 * (24/(h+1)));
+			eval7 = eval7 + (24/(h+1));
 		}
+		if (min > eval7) min = eval7;
+		if (max < eval7) max = eval7;
+		// eval += eval7*weight7;
+
 		for(int i = 0; i < board->getNumOppRings(); i++) {
 			board->convertBack(getRingVLR(2, 1, i), getRingVLR(2, 2, i), getRingVLR(2, 3, i), h, p);
-			eval = eval + (weight8 * (24/(h+1)));
+			eval8 = eval8 + (24/(h+1));
 		}
-		eval = eval + (weight9 * ringAttacks());
+		if (min > eval8) min = eval8;
+		if (max < eval8) max = eval8;
+		// eval += eval8*weight8;
+
+		eval9 = ringAttacks();
+		if (min > eval9) min = eval9;
+		if (max < eval9) max = eval9;
+		eval += eval9*weight9;
+
+		
 	}
-	else {
-		eval = eval + (weight1 * board->getNumDiscs()) + (weight2 * board->getNumOppDiscs());
-		eval = eval + (weight3 * board->getNumRings()) + (weight4 * board->getNumOppRings());
-		eval = eval + (weight5 * board->getRingsScored()*board->getRingsScored()) + (weight6 * board->getOppRingsScored());
+	else 
+	{
+		eval1 = board->getNumDiscs();
+		if (min > eval1) min = eval1;
+		if (max < eval1) max = eval1;
+		eval2 = board->getNumOppDiscs();
+		if (min > eval2) min = eval2;
+		if (max < eval2) max = eval2;
+		eval3 = board->getNumRings();
+		if (min > eval3) min = eval3;
+		if (max < eval3) max = eval3;
+		eval4 = board->getNumOppRings();
+		if (min > eval4) min = eval4;
+		if (max < eval4) max = eval4;
+		eval5 = board->getRingsScored()*board->getRingsScored();
+		if (min > eval5) min = eval5;
+		if (max < eval5) max = eval5;
+		eval6 = board->getOppRingsScored();
+		if (min > eval6) min = eval6;
+		if (max < eval6) max = eval6;
+		// eval = eval + (weight1 * eval1) + (weight2 * eval2);
+		// eval = eval + (weight3 * eval3) + (weight4 * eval4);
+		// eval = eval + (weight5 * eval5) + (weight6 * eval6);
 		
 		if (board->getRingsScored() == 3)
 			eval += numeric_limits<int>::max();
@@ -322,23 +341,84 @@ double YinshBot::evalFunction () {
 		vector< vector<int> > s2,e2;
 		board->row_detected_modified(s2,e2,-1,SequenceLength - 1);
 
-		eval += (weight10 * s1.size() * s1.size()) + (weight11 * s2.size() * s2.size());
+		eval10 = s1.size() * s1.size();
+		if (min > eval10) min = eval10;
+		if (max < eval10) max = eval10;
+		eval11 = s2.size() * s2.size();
+		if (min > eval11) min = eval11;
+		if (max < eval11) max = eval11;
+		// eval += (weight10 * eval10) + (weight11 * eval11);
 
 		for (int i=0; i<s1.size(); i++)
 		{
 			int controlled = board->row_marker_check(s1.at(i), e1.at(i),SequenceLength-1);
-			eval += (weight12 * controlled * controlled);
+			eval12 += controlled * controlled;
 		}
+		if (min > eval12) min = eval12;
+		if (max < eval12) max = eval12;
+		// eval += weight12*eval12;
 
 		for (int i=0; i<s2.size(); i++)
 		{
 			int oppControlled = SequenceLength - 1 - board->row_marker_check(s2.at(i), e2.at(i),SequenceLength-1);
-			eval += (weight13 * oppControlled * oppControlled);
+			eval13 += oppControlled * oppControlled;
 		}
+		if (min > eval13) min = eval13;
+		if (max < eval13) max = eval13;
+		// eval += weight13*eval13;
+
 		int s,c=0,c_opp=0;
 		s = board->all_marker_check(c,c_opp);
-		eval += weight14*c + weight15*c_opp;
+		if (min > eval14) min = eval14;
+		if (max < eval14) max = eval14;
+		eval14 = c;
+		if (min > eval15) min = eval15;
+		if (max < eval15) max = eval15;
+		eval15 = c_opp;
+		// eval += weight14*eval14 + weight15*eval15;
 		// eval = eval + (weight10 * discsOurControl()) + (weight11 * discsOppControl());
+		// cerr<<"normalized: "<<(eval1 - min)/(max-min)<<endl;
+		
 	}
+	f.push_back(eval1);
+	f.push_back(eval2);
+	f.push_back(eval3);
+	f.push_back(eval4);
+	f.push_back(eval5);
+	f.push_back(eval6);
+	f.push_back(eval7);
+	f.push_back(eval8);
+	f.push_back(eval9);
+	f.push_back(eval10);
+	f.push_back(eval11);
+	f.push_back(eval12);
+	f.push_back(eval13);
+	f.push_back(eval14);
+	f.push_back(eval15);
+	return f;
+}
+
+double YinshBot::evalFunction () {
+
+	double min = numeric_limits<int>::max(), max = numeric_limits<int>::min();
+	vector<double> w = getWeights(), f = getFeatures(min,max);
+	eval += -w.at(0)*(f.at(0) - min)/(max-min) + w.at(1)*(f.at(1) - min)/(max-min) + w.at(2)*(f.at(2) - min)/(max-min) 
+			-w.at(3)*(f.at(3) - min)/(max-min) + w.at(4)*(f.at(4) - min)/(max-min) - w.at(5)*(f.at(5) - min)/(max-min) + 
+			w.at(9)*(f.at(9) - min)/(max-min) - w.at(10)*(f.at(10) - min)/(max-min) + w.at(11)*(f.at(11) - min)/(max-min) - 
+			w.at(12)*(f.at(12) - min)/(max-min) + w.at(13)*(f.at(13) - min)/(max-min) - w.at(14)*(f.at(14) - min)/(max-min);
+	eval += w.at(6)*(f.at(6) - min)/(max-min) - w.at(7)*(f.at(7) - min)/(max-min) + w.at(8)*(f.at(8) - min)/(max-min);
+	eval = sigmoid(eval);
 	return eval;
+}
+
+double YinshBot::sigmoid(double eval)
+{
+	return 1/(1 + pow(2.71828,-1*eval));
+}
+
+vector<double> YinshBot::selfPlay(int p,vector<double> w)
+{
+	
+	vector<string> moves = moveList(perspective);
+
 }
